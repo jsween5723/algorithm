72758ea476212033f7a1604335d2d283436e980d
조현창 <136432473+jsween5723@users.noreply.github.com>
2025-11-17 22:59:49 +0900

    Time: 10 ms (2.3%), Space: 47.2 MB (21.87%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,29 @@
+
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for(int i = 0; i < flowerbed.length; i++) {
+            //flowerbed = 1 cnt countList add, cnt = 0;
+            //0 cnt++
+            //-1 +1 0 n-=1
+            // if (flowerbed[i] == 0) {
+            //     if (i == 0) cnt++;
+            //     if (i == (flowerbed.length - 1)) cnt++;
+            //     cnt++;
+            //     continue;
+            // }
+            // if (flowerbed[i] == 1 || i == flowerbed.length - 1) {
+            //     countList.add((cnt-1));
+            //     System.out.println(i + " " + countList.toString());
+            //     cnt = 0;
+            // }
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
+                n--;
+                flowerbed[i] = 1;
+            }
+        }
+        System.out.println(Arrays.toString(flowerbed));
+        System.out.println(n);
+        return n <= 0;
+    }
+}
